4bf953bca31608ba27d1dd1004daf59a491e6726
Corina <fluturelcorina@gmail.com>
2017-07-16 21:12:58 +0100

    Setting query request to Meaning cloud API

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "https://rubygems.org"
 ruby '2.4.0'
 
+
 gem 'sinatra'
 gem 'sinatra-flash'
 gem 'sinatra-partial'
--- app/app.rb
+++ app/app.rb
@@ -2,11 +2,14 @@ require 'sinatra/base'
 require 'sinatra/flash'
 require 'sinatra/partial'
 require_relative 'twitter_config'
+require_relative 'meaningcloud_config'
 
 class SentimentAnalysis < Sinatra::Base
   register Sinatra::Flash
   register Sinatra::Partial
+  set :partial_template_engine, :erb
   enable :sessions
+  set :session_secret, 'most secret'
   use Rack::MethodOverride
   
   get '/' do
@@ -20,12 +23,40 @@ class SentimentAnalysis < Sinatra::Base
   
   get '/search/results' do
     @tweets = TwitterApi.search(session[:search])
+    session[:tweets] = @tweets
     erb :'/search/results'
   end
   
   get '/search/analysis' do
+    p "ceva"
+    p session[:tweets]
+    result = []
     
-    redirect '/search/results'
+    session[:tweets].each do |tweet|
+      result << MeaningCloud.analyse(tweet[:text])
+    end
+    p result
+    erb :'/search/analysis'
   end
   
+  helpers do
+    
+    def edit_score_tag(score_tag)
+      case score_tag
+      when "P+"
+        "Strong positive"
+      when "P"
+        "Positive"
+      when "NEU"
+        "Neutral"
+      when "N"
+        "Negative"
+      when "N+"
+        "strong negative"
+      when "NONE"
+        "without sentiment"
+      end
+    end
+    
+  end
 end
