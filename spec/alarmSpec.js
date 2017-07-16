40d8467cc9d438ba261dae75c105f1e585c78707
MarySalemme <marysalemme1@gmail.com>
2017-07-16 19:09:47 +0100

    Adding methods to switch the alarm on and off

--- spec/alarmSpec.js
+++ spec/alarmSpec.js
@@ -15,11 +15,6 @@ describe("Alarm", function () {
     expect(alarm.alarmSwitch).toEqual(false);
   });
 
-  // it("can be switched on", function () {
-  //   alarm.switchOn();
-  //   expect(alarm.on).toEqual(true);
-  // });
-
   it("time can be set by the user", function () {
     expect(alarm.hour).toEqual(10);
     expect(alarm.min).toEqual(20);
@@ -27,6 +22,16 @@ describe("Alarm", function () {
 
   it("checks that the time entered by the user is valid", function () {
     expect(alarm.isValid()).toEqual(true);
+  });
+
+  it("can be switched on", function () {
+    alarm.turnOn();
+    expect(alarm.alarmSwitch).toEqual(true);
+  });
+
+  it("can be switched off", function () {
+    alarm.turnOff();
+    expect(alarm.alarmSwitch).toEqual(false);
   });
 
   it("checks whether time entered is equal to current time", function () {
--- src/alarm.js
+++ src/alarm.js
@@ -3,9 +3,11 @@ function Alarm(hour, min) {
   this.hour = hour;
   this.min = min;
   this.date = new Date();
-  this.date.setHours(hour);
-  this.date.setMinutes(min);
-}
+  if (this.isValid()) {
+    this.date.setHours(hour);
+    this.date.setMinutes(min);
+  }
+};
 
 Alarm.prototype.isValid = function () {
   return this.hour > 0
@@ -14,11 +16,20 @@ Alarm.prototype.isValid = function () {
   && this.min < 60;
 };
 
+Alarm.prototype.turnOn = function () {
+  this.alarmSwitch = true;
+};
+
+Alarm.prototype.turnOff = function () {
+  this.alarmSwitch = false;
+};
+
 Alarm.prototype.isTimeToSoundAlarm = function () {
   dateNow = new Date();
   return dateNow.getMinutes() === this.date.getMinutes()
          && dateNow.getHours() === this.date.getHours();
-}
+};
+
 Alarm.prototype.soundAlarm = function() {
   console.log("Riiiiiiing");
-}
+};
